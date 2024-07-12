6c53c3f7fb1574d9da19cfaa91d7646f4c9308d5
erenozgur98 <yigiterenozgur@outlook.com>
2024-07-12 00:38:44 -0400

    updates

--- middleware/auth0.middleware.js
+++ middleware/auth0.middleware.js
@@ -5,8 +5,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const validateAccessToken = auth({
-  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
   audience: process.env.AUTH0_AUDIENCE,
+  tokenSigningAlg: 'RS256'
 });
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const apiRoutes = require('./api')
 const movieceptionRoutes = require('./movieception')
 const { validateAccessToken } = require('../middleware/auth0.middleware')
 
+// Movieception Routes
+router.use('/api/v1.0', movieceptionRoutes)
+
 // API Routes
 router.use('/api', validateAccessToken, apiRoutes)
 
-// Movieception Routes
-router.use('/api/v1.0', movieceptionRoutes)
 
 module.exports = router
